27c4ad08bdb6c512440eccfd1039f7e970f8f1a4
oilenosreg <99894388+oilenosreg@users.noreply.github.com>
2022-06-29 18:31:53 -0500

    Update managers.py

--- usuarios/managers.py
+++ usuarios/managers.py
@@ -1,28 +1,5 @@
 from django.contrib.auth.base_user import BaseUserManager
 
-
-# class CustomUserManager(BaseUserManager):
-#     def create_user(self, email, password, **extra_fields):
-#         if not email:
-#             raise ValueError('El correo electrónico es necesario.')
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, **extra_fields)
-#         user.set_password(password)
-#         user.save()
-#         return user
-
-#     def create_superuser(self, email, password, **extra_fields):
-#         extra_fields.setdefault('is_staff', True)
-#         extra_fields.setdefault('is_superuser', True)
-#         extra_fields.setdefault('is_active', True)
-
-#         if extra_fields.get('is_staff') is not True:
-#             raise ValueError('El supersusuario debe tener is_staff = True.')
-#         if extra_fields.get('is_superuser') is not True:
-#             raise ValueError('El superusuario debe tener is_superuser=True.')
-#         return self.create_user(email, password, **extra_fields)
-
-
 class CustomUserManager(BaseUserManager):
     def create_user(self, dni, password, **extra_fields):
         if not dni:
